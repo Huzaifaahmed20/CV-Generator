1962b304af592fcc0e85e3b865596c6ba3bc04ce
Huzaifa <ahmedhuzaifa13@gmail.com>
2017-05-28 16:35:30 +0500

    JavaScript file

---
+++ app.js
@@ -0,0 +1,53 @@
+// var greetuser = document.getElementById('greet');
+// console.log(greetuser.innerHTML);
+// greetuser.innerHTML= 'hello huzaifa';
+
+// var userInput = prompt("Enter ur name");
+// var userName = document.getElementById('username');
+// userName.innerHTML = userInput;
+
+            
+var nameinput = prompt("Enter Your Name");
+var personName = document.getElementById('name');
+personName.innerHTML = nameinput;
+
+var fatherNameInput = prompt("Enter Your Father Name");
+var fatherName = document.getElementById('fname');
+fatherName.innerHTML = fatherNameInput;
+
+var yearofedu = prompt("Enter Year of education");
+var years = document.getElementById('year');
+years.innerHTML = yearofedu;
+
+var grades = +prompt("Your GPA");
+var againgrade  =  document.getElementById('grade');
+againgrade.innerHTML = grades;
+
+var univer = prompt("Enter Your uni");
+var universe  = document.getElementById('uni');
+universe.innerHTML = univer;
+var skil1 = prompt("Enter your Skills");
+var skill1 = document.getElementById('skills1');
+skill1.innerHTML = skil1;
+
+var skil2 = prompt("Enter your Skills");
+var skill2 = document.getElementById('skills2');
+skill2.innerHTML = skil2;
+
+var skil3 = prompt("Enter your Skills");
+var skill3 = document.getElementById('skills3');
+skill3.innerHTML = skil3;
+
+var phonNo= prompt("Your Cell No");
+var phn = document.getElementById('phnno');
+phn.innerHTML=phonNo;
+
+var emails=prompt("Enter your E-Mail");
+var emailadd  = document.getElementById('email');
+emailadd.innerHTML=emails;
+
+var add=prompt("Enter your Address");
+var addres  = document.getElementById('address');
+addres.innerHTML=add;
+
+	
